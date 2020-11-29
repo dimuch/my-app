ed76b8b01c52e587b5e4ed32b94fb0cac4948cc4
dmytro.kostylov <dmytro.kostylov@gmail.com>
2020-11-29 22:33:18 +0200

    seems work

--- test_admc/my-app/src/App.jsx
+++ test_admc/my-app/src/App.jsx
@@ -5,27 +5,6 @@ import './App.scss';
 
 let yStart = 0, yEnd = 0, diff = 0, isSplitterSelected, h;
 
-// const useAnimationFrame = callback => {
-//   // Use useRef for mutable variables that we want to persist
-//   // without triggering a re-render on their change
-//   const requestRef = useRef(getContentHeight());
-//   const previousTimeRef = useRef(getContentHeight());
-//
-//   const animate = time => {
-//     if (previousTimeRef.current !== undefined) {
-//       const deltaTime = time - previousTimeRef.current;
-//       callback(deltaTime)
-//     }
-//     previousTimeRef.current = time;
-//     requestRef.current = requestAnimationFrame(animate);
-//   }
-//
-//   useEffect(() => {
-//     requestRef.current = requestAnimationFrame(animate);
-//     return () => cancelAnimationFrame(requestRef.current);
-//   }, []); // Make sure the effect runs only once
-// }
-
 function getContentHeight() {
   const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   return height / 2
